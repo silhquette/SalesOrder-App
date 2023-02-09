43b27ca5ef24e34493b89bdd61afd1a84ce7de50
silhquette <muhammadqiyam03@gmail.com>
2023-02-09 13:49:12 +0700

    deploy#1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,21 +17,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('password')
-        ]);
-
-        $this->call([
-            ProductSeeder::class,
-        ]);
+        
     }
 }
